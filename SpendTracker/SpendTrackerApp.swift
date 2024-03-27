a354b54b31a19ea798c24edaa701fbba0174f00c
Caroline Chan <carolinechan304@gmail.com>
2024-03-27 11:09:34 +0700

    Initial Commit

---
+++ SpendTracker/SpendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackerApp.swift
+//  SpendTracker
+//
+//  Created by Caroline Chan on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
